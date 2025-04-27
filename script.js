9d1f2968c8977bb7c14b83fd4fea7618e6c83b2e
Shubham Singh <128978409+aryansingh822002@users.noreply.github.com>
2025-04-27 16:33:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+  if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+    return 366;
+  } else {
+    return 365;
+  } 
+}
+
+let year = prompt("Enter a year between 1 and 9999:");
+year = parseInt(year);
+
+alert(daysOfAYear(year));
